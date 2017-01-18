64db447862eddea37ec5afbefaca70b957f62b02
DreamWFJ <wfj_sc@163.com>
2017-01-18 18:51:05 +0800

    alter test_unittest

--- StandardLib/test_unittest.py
+++ StandardLib/test_unittest.py
@@ -6,27 +6,40 @@
 # CreateTime : 2017-01-18 17:16
 # ===================================
 
+import random
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+class TestSequenceFunctions(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def setUp(self):
+        self.seq = range(10)
+        print "call setup"
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def tearDown(self):
+        print "call teardown"
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
 
 if __name__ == '__main__':
     unittest.main()
     # 可以使用下面的2行代替 unittest.main()
-    # suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+    # suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
     # unittest.TextTestRunner(verbosity=2).run(suite)
     
